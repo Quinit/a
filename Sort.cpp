b1ba6ad6fbfc3cb05df607727776cf951f5275fa
Quinit <tante2020@qq.com>
2023-12-02 19:43:28 +0800

    modified:   Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -23,8 +23,6 @@ int BubbleSort(SqList &L,SqList &L_Sort){
     return 0;
 }
 
-int InsertFind(SqList &L,int x);
-
 int InsertSort(SqList &L){
     int j = 0;//已排序的队列长度
     for(int i = 1;i < L.length; i++){
@@ -42,3 +40,80 @@ int InsertSort(SqList &L){
     }
     return 0;
 }
+
+int Parction(SqList &L, int low, int high){
+    int pivot = L.data[low];
+    while(low < high){
+        while(low < high && L.data[high] >= pivot) --high;
+        L.data[low] = L.data[high];
+        while(low < high && L.data[low] <= pivot) ++low;
+        L.data[high] = L.data[low];
+    }
+    L.data[low] = pivot;
+    return low;
+}
+
+int QuickSort(SqList &L, int low, int high){
+    if(low < high){
+        int pivotpos = Parction(L,low,high);
+        QuickSort(L,low,pivotpos-1);
+        QuickSort(L,pivotpos+1,high);
+    }
+}
+
+int HeadAdjust(SqList L, int k, int len){
+    L.data[0] = L.data[k];
+    for(int i = 2 * k; i <= len; i *= 2){
+        if(i < len && L.data[i] < L.data[i+1])  i ++;
+        if(L.data[0] >= L.data[i])   break;
+        else{
+            L.data[k] = L.data[i];
+            k = i;
+        }
+    }
+    L.data[k] = L.data[0];
+}
+
+int BulidMaxHeap(SqList L, int len){
+    for(int i = len/2; i > 0; i--)
+    HeadAdjust(L,i,len);
+}
+
+int HeapSort(SqList L, int len){
+    BulidMaxHeap(L, len);
+    for(int i = len; i > 1; i --){
+        swap(L.data[i], L.data[1]);
+        HeadAdjust(L, 1, i-1);
+    }
+}
+
+int Merge(SqList L, int low, int mid, int high){
+    SqList B;
+    int i, j, k;
+    //copy
+    for(k = low; k<= high; k++){
+        B.data[k] = L.data[k];
+    }
+    //
+    for(i = low, j = mid + 1, k = i; i <= mid && j<= high; k++){
+        if(L.data[k] <= B.data[j])  L.data[k] = B.data[i++];
+        else L.data[k] = B.data[j++];
+    }
+    while(i <= mid)     L.data[k++] = B.data[i++];
+    while(j <= high)    L.data[k++] = B.data[j++];
+};
+
+int MergeSort(SqList L, int low, int high){
+    if(low < high){
+        int mid = (low + high)/2;
+        MergeSort(L, low, mid);
+        MergeSort(L, mid + 1, high);
+        Merge(L, low, mid, high);
+    }
+}
+
+
+
+
+
+
